67a75274eb55927719c51601289cdd0774840a20
jProgr <8537691@gmail.com>
2018-03-28 16:10:45 -0600

    Add: Get tweets from a default list of users or from config file

--- accounts.js
+++ accounts.js
@@ -1,3 +1,18 @@
-module.exports = {
-  userNames: ['MagicRealismBot', 'tinycarebot', 'metaprophet']
-};
+const fs = require('fs');
+
+let accounts;
+const configFile = __dirname + '/config';
+if(fs.existsSync(configFile))
+{
+  accounts = {
+    userNames: fs.readFileSync(configFile, 'utf-8').trim().split('\n')
+  }
+}
+else
+{
+  accounts = {
+    userNames: ['MagicRealismBot', 'tinycarebot', 'metaprophet']
+  };
+}
+
+module.exports = accounts;
