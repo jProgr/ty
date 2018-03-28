17ef070d8a2091b971531967a22decf323721470
jProgr <8537691@gmail.com>
2018-03-28 09:35:59 -0600

    Gets tweet

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var twitter = require('twitter');
 var credentials = require('./auth.js');
+
+function presentTweet(error, tweet, response)
+{
+  if(error) throw error;
+  console.log(tweet[0].text);
+}
+
+const twitterClient = new twitter(credentials);
+
+twitterClient.get('statuses/user_timeline',
+                  {user_id: 'MagicRealismBot', count: 1},
+                  presentTweet);
